6a3711bb9f774393291d55829640da592141c52e
aa <aa@aa>
2022-08-17 23:56:27 +0000

    initial commit pcd and ply vis

--- TestVisualizer.cpp
+++ TestVisualizer.cpp
@@ -29,11 +29,66 @@
 #include <thread>
 
 #include <open3d/Open3D.h>
+namespace {
+bool read_and_visualize_mesh(const std::string& file_name) {
+  auto mesh_ptr = std::make_shared<open3d::geometry::TriangleMesh>();
+  if (open3d::io::ReadTriangleMesh(file_name, *mesh_ptr)) {
+    open3d::utility::LogWarning("Successfully read %s\n", file_name.c_str());
+    if (mesh_ptr->triangles_.size() == 0) {
+      open3d::utility::LogWarning("Contains 0 triangles, will read as point cloud\n");
+      return false;
+    }
+  } else {
+    open3d::utility::LogError("Failed to read %s\n\n", file_name.c_str());
+    return false;
+  }
+  mesh_ptr->ComputeVertexNormals();
+  open3d::visualization::DrawGeometries({mesh_ptr}, "Mesh " + file_name, 1600, 900);
+  return true;
+}
 
-int main(int argc, char *argv[]) {
+bool read_and_visualize_point_cloud(const std::string& file_name) {
+  auto cloud_ptr = std::make_shared<open3d::geometry::PointCloud>();
+  if (open3d::io::ReadPointCloud(file_name, *cloud_ptr)) {
+    open3d::utility::LogWarning("Successfully read %s\n", file_name.c_str());
+  } else {
+    open3d::utility::LogError("Failed to read %s\n\n", file_name.c_str());
+    return false;
+  }
+  cloud_ptr->NormalizeNormals();
+  open3d::visualization::DrawGeometries({cloud_ptr}, "PointCloud " + file_name, 1600, 900);
+  return true;
+}
+
+void visualize_sphere_mesh() {
   auto sphere = open3d::geometry::TriangleMesh::CreateSphere(1.0);
   sphere->ComputeVertexNormals();
   sphere->PaintUniformColor({0.0, 1.0, 0.0});
   open3d::visualization::DrawGeometries({sphere});
+}
+
+bool auto_visualize(const std::string& file_name) {
+  bool rc = true;
+  if (!read_and_visualize_mesh(file_name)) {
+    rc = read_and_visualize_point_cloud(file_name);
+  }
+  return rc;
+}
+}
+
+int main(int argc, char *argv[]) {
+  open3d::utility::SetVerbosityLevel(open3d::utility::VerbosityLevel::Debug);
+
+  if (argc < 2) {
+    visualize_sphere_mesh();
+  } else {
+    std::string file_name(argv[1]);
+    std::string file_type = file_name.substr(static_cast<size_t>(file_name.size()) - 3);
+    if (file_type == "pcd") {
+      read_and_visualize_point_cloud(file_name);
+    } else if (file_type == "ply") {
+      read_and_visualize_mesh(file_name);
+    }
+  }
   return 0;
 }
